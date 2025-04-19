a103be872ebdecc41dde1c8b6ad84903024e7d5e
eduardo moraes <eduardodmoraes@icloud.com>
2025-04-19 01:21:42 -0300

    questão 3 resolvida com cálculo de IMC e switch case

--- src/js/script.js
+++ src/js/script.js
@@ -20,6 +20,33 @@ console.log("x == 10:", x == 10);
 console.log("x >= y:", x >= y);
 console.log("y >= x:", y >= x);
 
+console.log("===== Questão 3 =====");
+
+let peso = 76;
+let altura = 1.91;
+
+let imc = peso / (altura * altura);
+console.log("IMC:", imc.toFixed(2));
+
+let classificacao = 
+    imc < 18.5 ? "abaixo":
+    imc <= 24.9 ? "ideal":
+    "acima";
+
+switch (classificacao){
+    case "abaixo":
+        console.log("Você está abaixo do peso.");
+        break;
+    case "ideal":
+        console.log("Você está no peso ideal.");
+        break;
+    case "acima":
+        console.log("Você está acima do peso.")
+        break;    
+}
+
+
+
 
 
 
