ae6bf40faeaddc1e6a0bb54083734615f284e7c4
eduardo moraes <eduardodmoraes@icloud.com>
2025-04-19 00:50:37 -0300

    questão 1 resolvida com variáveis undefined, vazia e com valor

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+// 1. Variável com valor undefined (sem valor atribuído)
+let a;
+console.log(a)
+
+let b = "";
+console.log(b);
+
+let c = "Novus vinheria";
+console.log(c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
